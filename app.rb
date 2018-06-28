55450f870519ec35c09b4b9fe458b37c7013ec5c
isabella3412 <izzybella17@icloud.com>
2018-06-28 20:57:58 +0000

    ADDED VAISH APP.RB

--- app.rb
+++ app.rb
@@ -1,15 +1,37 @@
 require 'bundler'
 Bundler.require
-#require_relative 'models/questions'
 
 class MyApp < Sinatra::Base
 
  get '/' do
-  erb :whereintheworld
+   erb :whereintheworld
  end
 
- get '/africa' do
+ get '/views/africa' do
    erb :africa
  end
 
+ get '/views/asia' do
+   erb :asia
+ end
+
+ get '/views/australia' do
+   erb :australia
+ end
+
+ get '/views/europe' do
+   erb :europe
+ end
+
+# get '/views/france'
+#   erb :france
+# end
+
+  get '/views/northamerica' do
+    erb :northamerica
+  end
+
+  get '/views/southamerica' do
+    erb :southamerica
+  end
 end
